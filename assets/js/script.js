daef39167a0f3f9d5389391af0a7d614b17cd077
Hannah Moody <hlmooney98@gmail.com>
2025-01-28 11:41:20 +0000

    added multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
+    } else if(gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType} Aborting!`;
@@ -56,7 +58,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];  
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else  {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -87,8 +91,11 @@ function displaySubtractionQuestion() {
 
 }
 
-function displayMultiplicationQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+
 }
 
 function displayDivisionQuestion() {
