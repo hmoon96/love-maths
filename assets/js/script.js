3f3959bba7d69d9d768bbd6ed039fb950522aa02
Hannah Moody <hlmooney98@gmail.com>
2025-01-28 14:14:08 +0000

    added the division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if(gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2)
     } else {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType} Aborting!`;
@@ -74,7 +76,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else  {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }    else  {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -115,6 +119,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivisionQuestion() {
-
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1*operand2;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
